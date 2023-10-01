9b9ba49818aae3571ec2b7de1befc046b33ab14f
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2023-09-30 21:56:57 -0400

    some cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -53,3 +53,41 @@ fn align_up(num: usize, to: usize) -> usize {
     ((num + to - 1) / to) * to
 }
 
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+
+    #[test]
+    fn encode_str_b64() {
+        let tests = HashMap::from([
+            ("Man", "TWFu"),
+            ("Ma", "TWE="),
+            ("M", "TQ=="),
+            ("light work.", "bGlnaHQgd29yay4="),
+            ("light work", "bGlnaHQgd29yaw=="),
+            ("light wor", "bGlnaHQgd29y"),
+            ("light wo", "bGlnaHQgd28="),
+            ("light w", "bGlnaHQgdw=="),
+        ]);
+        for (test, expected) in &tests {
+            let b64 = u8_to_b64(test.as_bytes()).unwrap_or(String::from(""));
+            assert_eq!(b64, expected.to_owned());
+        }
+    }
+
+    #[test]
+    fn encode_hex_b64() {
+        let tests = HashMap::from([
+            (
+            "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+            ),
+        ]);
+        for (test, expected) in &tests {
+            let to_u8 = hex_to_u8(test).unwrap_or(Vec::new());
+            let b64 = u8_to_b64(&to_u8).unwrap_or(String::from(""));
+            assert_eq!(b64, expected.to_owned());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,21 @@
 use std::error::Error;
 use cryptopals::{hex_to_u8, u8_to_b64};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    println!("{}", u8_to_b64(&hex_to_u8(hex)?)?);
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+fn main() -> Result<()> {
+    println!("challenge 1: {}", c1()?);
+
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-    use super::*;
+fn c1() -> Result<String> {
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    Ok(u8_to_b64(&hex_to_u8(hex)?)?)
+}
 
-    #[test]
-    fn encode_str_b64() {
-        let tests = HashMap::from([
-            ("Man", "TWFu"),
-            ("Ma", "TWE="),
-            ("M", "TQ=="),
-            ("light work.", "bGlnaHQgd29yay4="),
-            ("light work", "bGlnaHQgd29yaw=="),
-            ("light wor", "bGlnaHQgd29y"),
-            ("light wo", "bGlnaHQgd28="),
-            ("light w", "bGlnaHQgdw=="),
-        ]);
-        for (test, expected) in &tests {
-            let b64 = u8_to_b64(test.as_bytes()).unwrap_or(String::from(""));
-            assert_eq!(b64, expected.to_owned());
-        }
-    }
-
-    #[test]
-    fn encode_hex_b64() {
-        let tests = HashMap::from([
-            (
-            "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
-            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-            ),
-        ]);
-        for (test, expected) in &tests {
-            let to_u8 = hex_to_u8(test).unwrap_or(Vec::new());
-            let b64 = u8_to_b64(&to_u8).unwrap_or(String::from(""));
-            assert_eq!(b64, expected.to_owned());
-        }
-    }
+fn c2() -> Result<()> {
+    let hex1_u8 = hex_to_u8("1c0111001f010100061a024b53535009181c")?;
+    let hex2_u8 = hex_to_u8("686974207468652062756c6c277320657965")?;
+    Ok(())
 }
