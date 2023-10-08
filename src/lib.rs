d190b0c0cb13e8d813ff09f3a7708244eb1b5fb0
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2023-10-08 17:20:17 -0400

    finish s1c3

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
 use bitvec::prelude::*;
+use std::collections::HashMap;
 use std::error::Error;
 use std::str;
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+pub const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+// This also encodes the space character (' ') on index 0
+// with a frequency almost twice as the most frequent ('e')
+// https://web.archive.org/web/20170918020907/http://www.data-compression.com/english.html
+const LETTER_FREQUENCY: [f64; 27] = [
+    0.1918182, 0.0651738, 0.0124248, 0.0217339, 0.0349835, 0.1041442, 0.0197881, 0.0158610,
+    0.0492888, 0.0558094, 0.0009033, 0.0050529, 0.0331490, 0.0202124, 0.0564513, 0.0596302,
+    0.0137645, 0.0008606, 0.0497563, 0.0515760, 0.0729357, 0.0225134, 0.0082903, 0.0171272,
+    0.0013692, 0.0145984, 0.0007836,
+];
 
-fn encode_table(alphabet: &str) -> [u8; 64] {
+pub fn encode_table(alphabet: &str) -> [u8; 64] {
     alphabet.as_bytes().try_into().unwrap()
 }
 
@@ -52,6 +62,21 @@ fn align_up(num: usize, to: usize) -> usize {
     ((num + to - 1) / to) * to
 }
 
+pub fn calculate_frequency_score(input: &[u8]) -> f64 {
+    let mut total_score: f64 = 0.0;
+    let mut counter: HashMap<char, usize> = HashMap::new();
+    for c in input {
+        if (*c as char).is_ascii_alphabetic() || (*c as char) == ' ' {
+            let count = counter.entry(*c as char).or_insert(0);
+            *count += 1;
+        }
+    }
+    for (ch, num) in counter {
+        total_score += LETTER_FREQUENCY[(ch as usize % 32) as usize] * num as f64;
+    }
+    total_score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,9 +85,9 @@ mod tests {
     #[test]
     fn encode_str_b64() {
         let tests = HashMap::from([
-            ("Man", "TWFu"),
-            ("Ma", "TWE="),
-            ("M", "TQ=="),
+            ("Cat", "Q2F0"),
+            ("Ca", "Q2E="),
+            ("C", "Qw=="),
             ("light work.", "bGlnaHQgd29yay4="),
             ("light work", "bGlnaHQgd29yaw=="),
             ("light wor", "bGlnaHQgd29y"),
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use cryptopals::{hex_to_u8, u8_to_b64, Result};
+use cryptopals::{calculate_frequency_score, hex_to_u8, u8_to_b64, Result, ALPHABET};
+use std::str;
 
 fn main() -> Result<()> {
+    println!(
+        "{:?}",
+        c3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?
+    );
+
     Ok(())
 }
 
@@ -12,13 +18,40 @@ fn c2(hex1: &str, hex2: &str) -> Result<String> {
     let mut hex1_u8 = hex_to_u8(hex1)?;
     let hex2_u8 = hex_to_u8(hex2)?;
 
-    hex1_u8.iter_mut()
-            .zip(hex2_u8.iter())
-            .for_each(|(x1, x2)| *x1 ^= *x2);
+    hex1_u8
+        .iter_mut()
+        .zip(hex2_u8.iter())
+        .for_each(|(x1, x2)| *x1 ^= *x2);
 
-    Ok(hex1_u8.iter().map(|b| format!("{:02x}", b)).collect::<String>())
+    Ok(hex1_u8
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>())
 }
 
+fn c3(input: &str) -> Result<(char, String, f64)> {
+    let s = hex_to_u8(input)?;
+    let z: Vec<(u8, Vec<u8>)> = ALPHABET
+        .as_bytes()
+        .iter()
+        .map(|x| (*x, s.iter().map(|inp| *x ^ *inp).collect::<Vec<u8>>()))
+        .collect();
+
+    let y = z
+        .iter()
+        .map(|(k, chars)| (k, chars, calculate_frequency_score(chars)))
+        .fold(('a', "".to_owned(), 0.0), |acc, (k, c, v)| {
+            let i = (*k as char, str::from_utf8(c).unwrap().to_owned(), v);
+            let max = f64::max(acc.2, v);
+            if max == v {
+                i
+            } else {
+                (acc.0 as char, acc.1, acc.2)
+            }
+        });
+
+    Ok(y)
+}
 
 #[cfg(test)]
 mod tests {
@@ -29,8 +62,7 @@ mod tests {
         let c1 = c1("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")?;
         assert_eq!(
             c1,
-            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-                .to_owned(),
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_owned(),
         );
         Ok(())
     }
@@ -41,10 +73,14 @@ mod tests {
             "1c0111001f010100061a024b53535009181c",
             "686974207468652062756c6c277320657965",
         )?;
-        assert_eq!(
-            c2,
-            "746865206b696420646f6e277420706c6179".to_owned()
-        );
+        assert_eq!(c2, "746865206b696420646f6e277420706c6179".to_owned());
+        Ok(())
+    }
+
+    #[test]
+    fn challenge_3() -> Result<()> {
+        let c3 = c3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
+        assert_eq!(c3.0, 'X');
         Ok(())
     }
 }
