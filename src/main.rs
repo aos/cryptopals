4e991fc349ab298aac1f2bfd96c0a6520cc99a8d
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2023-10-19 15:32:33 -0400

    finish s1c5

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use cryptopals::{calculate_frequency_score, hex_to_u8, Base64, Result, ALPHABET}
 use std::str;
 
 fn main() -> Result<()> {
-    println!("{:?}", c4("./inputs/s1c4_input.txt")?);
+    println!("{}", c5("Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal")?);
+
     Ok(())
 }
 
@@ -83,6 +85,13 @@ fn c4(filename: &str) -> Result<(char, String, String, f64)> {
     Ok(max_line)
 }
 
+fn c5(input: &str) -> Result<String> {
+    Ok(input.bytes()
+        .zip("ICE".bytes().cycle())
+        .map(|(first, second)| format!("{:02x}", first ^ second))
+        .collect::<String>())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -120,6 +129,17 @@ mod tests {
         assert_eq!(
             c4.1,
             "7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn challenge_5() -> Result<()> {
+        let c5 = c5("Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal")?;
+        assert_eq!(
+            c5,
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
         );
         Ok(())
     }
